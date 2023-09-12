4b8a1ba03976dac2fcd3d28853f572774466f213
“ChristosM22” <christosmalamas@outlook.com>
2023-09-12 16:09:41 +0300

    feat: Added inline documentation

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,50 +1,59 @@
+import React from 'react';
 import {
-    useAccount,
-    useConnect,
-    useDisconnect,
-    useEnsAvatar,
-    useEnsName,
-  } from 'wagmi'
-import Card from './Card'
-  
-  export function Profile() {
-    const { address, connector, isConnected } = useAccount()
-    const { data: ensAvatar } = useEnsAvatar({ address })
-    const { data: ensName } = useEnsName({ address })
-    const { connect, connectors, error, isLoading, pendingConnector } =
-      useConnect()
-    const { disconnect } = useDisconnect()
-  
-    if (isConnected) {
-      return (
-       
-        <Card connectorName={connector?.name}  address={address} disconnect={disconnect}/>
-       
-         
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+} from 'wagmi';
+import Card from './Card';
 
-      
-      )
-    }
-  
+/**
+ * Profile Component for displaying user's wallet information and connection status.
+ * @returns {JSX.Element} The rendered Profile component.
+ */
+export function Profile() {
+  // Retrieve user account information, connection status, and connectors.
+  const { address, connector, isConnected } = useAccount();
+
+  // Retrieve the user's EnsAvatar and EnsName.
+  const { data: ensAvatar } = useEnsAvatar({ address });
+  const { data: ensName } = useEnsName({ address });
+
+  // Connect to wallets, handle disconnections, and manage loading/error states.
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+  const { disconnect } = useDisconnect();
+
+  if (isConnected) {
+    // User is connected, display the wallet card.
     return (
-      <div className='w-full flex flex-col items-center gap-10'>
-        <h2 className='mb-4'>Connecting Wallet with Wagmi</h2>
-        {connectors.map((connector) => (
-            <button      disabled={!connector.ready}
-            key={connector.id}
-            onClick={() => connect({ connector })} className="border w-[350px] justify-center items-center  flex rounded-lg shadow-lg shadow-emerald-200 border-white border-t-2 border-l-2 hover:rounded">
+      <Card connectorName={connector?.name} address={address} disconnect={disconnect} />
+    );
+  }
 
-          <h3 className="text-center text-white py-5 col-end-4 pl-2"> {connector.name}
+  // User is not connected, display wallet connection options.
+  return (
+    <div className='w-full flex flex-col items-center gap-10'>
+      <h2 className='mb-4'>Connecting Wallet with Wagmi</h2>
+      {connectors.map((connector) => (
+        <button
+          disabled={!connector.ready}
+          key={connector.id}
+          onClick={() => connect({ connector })}
+          className="border w-[350px] justify-center items-center flex rounded-lg shadow-lg shadow-emerald-200 border-white border-t-2 border-l-2 hover:rounded"
+        >
+          <h3 className="text-center text-white py-5 col-end-4 pl-2">
+            {connector.name}
             {!connector.ready && ' (unsupported)'}
             {isLoading &&
               connector.id === pendingConnector?.id &&
-              ' (connecting)'}</h3>
-      </button>
-         
-        ))}
-  
-        {error && <div>{error.message}</div>}
-      </div>
-    )
-  }
-  
+              ' (connecting)'}
+          </h3>
+        </button>
+      ))}
+
+      {error && <div>{error.message}</div>}
+    </div>
+  );
+}
